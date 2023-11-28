bb842d34584cf10f7296984d17b93b55792e8f15
John Osborne <jwosborne321@gmail.com>
2023-11-28 21:26:00 +0000

    Updated Validation

--- main.py
+++ main.py
@@ -123,11 +123,11 @@ def usernameScreen(level):
             userNameArray.append(i[0])
 
         if userUsername in userNameArray: # If username is taken, tell the user to re-enter
-            invalidUsername_label = customtkinter.CTkLabel(usernameScreen_window, text="Username already taken. Please enter another", font=("Comic Sans MS", 17), fg_color=("#EBEBEA", "#252424"))
+            invalidUsername_label = customtkinter.CTkLabel(usernameScreen_window, text="Username already taken. Please enter another.", font=("Comic Sans MS", 17), fg_color=("#EBEBEA", "#252424"))
             invalidUsername_label.place(relx=0.5, rely=0.75, anchor="center")            
         
         elif userUsername not in userNameArray:
-            storeUsername(userUsername) # If the username is valid then store it in the datbase
+            storeUsername(userUsername) # If the username is valid (not already taken) then call the function to store it in the database
 
         db.close()
 
